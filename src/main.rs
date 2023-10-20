f5c9e4d182db7238ad1d3b070d422d43f35d0948
Gwendolyn Krezel <87609785+Gwk68@users.noreply.github.com>
2023-10-20 13:08:16 -0400

    Lets goo

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-const there_is_shit : u8 = 1;
-
-pub trait Entity {
-  
-}
-
+use bevy::prelude::*;
 
 fn main() {
-    println!("Hello, world!");
+    App::new().add_plugins(DefaultPlugins).run();
 }
+
+#[derive(Component)]
+struct WorldItem {
+    item_id: u8,
+}
+
+#[derive(Component)]
+struct Buildable {}
+
+#[derive(Component)]
+struct Belt {
+    // items on belt should be ordered like a queue
+    items_on_belt:[Entity;2],
+    
+}
+
+
